2f9e89173b4108696023dc51c0f8af898697bc19
Jewcub <xiaokourou@gmail.com>
2021-12-06 12:31:48 +0800

    Bio update

--- src/layouts/sections/index/AboutSection.vue
+++ src/layouts/sections/index/AboutSection.vue
@@ -23,16 +23,17 @@
             </b-row>
             <b-row>
               <p class="about-p mt-n3">
-                I can make websites and web apps. Starting with some UX
-                brainstorming techniques, I make basic designs using Photoshop
-                and Figma, create Javascript front-ends mostly with Vue.js, and
-                build backends mostly with Python Flask or Node.js Koa/Express,
-                and connect to PostgreSQL, MondoDB, and ThreadsDB databases.
+                I can make full-stack websites and web apps. Starting with some
+                UX brainstorming techniques, I make basic designs using
+                Photoshop and Figma. I create Javascript(usually Typescript)
+                front-ends mostly with React and Vue. I build backends mostly
+                with Node.js Koa/Express, and also Python Flask. I connect them
+                to PostgreSQL, MondoDB, and Textile ThreadsDB databases.
               </p>
               <p class="about-p mb-5">
-                I’ve used a wide variety of frameworks and libraries like
+                I’ve used a wide variety of frameworks and libraries like Redux,
                 Bootstrap, JQuery, Nuxt and Gridsome. I’ve solved tricky logical
-                problems like a handwritten cloud sync merge with conflict
+                problems like handwritten cloud sync merges with custom conflict
                 resolution. I’ve explored cutting edge technologies like
                 distributed and decentralized architecture and integrated them
                 in my projects in a meaningful way.
@@ -44,7 +45,7 @@
         <b-col id="education-acumen" class="home-page-section" lg="6">
           <b-container class="max-width-400"
             ><b-row class="ml-n4"
-              ><b-col style="z-index:500" cols="6" class="ml-n3 mt-3  ">
+              ><b-col style="z-index: 500" cols="6" class="ml-n3 mt-3">
                 <p class="title sm-txt mb-n2">Education</p>
                 <p class="title lg-txt">Acumen</p> </b-col
               ><b-col cols="6"
@@ -84,6 +85,12 @@
         <b-col id="my-story" class="home-page-section" lg="6">
           <b-container class="max-width-400">
             <b-row>
+              <p class="about-p">
+                I'm currently working as a full-time front-end software engineer
+                at
+                <a href="https://kava.io/">Kava Labs</a>, where I work on
+                implementing blockchain solutions for decentralized finance.
+              </p>
               <p class="about-p mt-5">
                 I’m from Ithaca, NY and live in Fuzhou, China. I’m fluent in
                 Chinese. I started a Mexican restaurant by selling burritos on
@@ -92,28 +99,13 @@
                 establishment in our city of 8 million.
               </p>
               <p class="about-p">
-                I studied Psychology and Ceramic Art in college, which remain
-                interests of mine. I love playing guitar and piano. I used to
-                love cooking.
+                I studied Psychology and Ceramic Art in college. I love playing
+                guitar and piano. I used to love cooking.
               </p>
-              <p class="about-p">
-                I'm currently working at
-                <a href="https://en.fjmu.edu.cn/">Fujian Medical University</a>
-                as an English Language Instructor, and at Bitcoin
-                <a href="https://www.mempool.com/">mining</a> and
-                <a href="https://dotwallet.com">wallet</a> company
-                <a href="https://www.boquaninc.com"
-                  >Fuzhou Boquan Network Technology Co., Ltd</a
-                >
-                doing developer outreach, front-end development, and translation
-                of technical documentation.
-              </p></b-row
-            >
-            <b-row class="mt-3 ml-0 mr-0 ">
+            </b-row>
+            <b-row class="mt-3 ml-0 mr-0">
               <b-col cols="4" class="p-0">
-                <p class="title sm-txt mb-n2">
-                  What a long strange
-                </p>
+                <p class="title sm-txt mb-n2">What a long strange</p>
                 <p class="title lg-txt mt-n2">trip</p>
                 <div class="bottom-space"></div>
               </b-col>
@@ -135,9 +127,7 @@
             <b-row class="ml-n4 contact-info-row">
               <p class="title sm-txt mt-4">get in</p>
               <p class="title lg-txt mt-n2 ml-1">touch</p>
-              <p class="about-p mt-n5">
-                jacob@jacobcohen-rosenthal.me
-              </p>
+              <p class="about-p mt-n5">jacob@jacobcohen-rosenthal.me</p>
               <div id="contact" class="bottom-space"></div>
             </b-row>
           </b-container>
@@ -159,12 +149,12 @@ export default {
   },
   methods: {
     startScroll() {
-      var elmnt = document.getElementById('contact');
+      var elmnt = document.getElementById("contact");
       elmnt.scrollIntoView();
     },
   },
   mounted() {
-    if (window.location.hash === '#contact') {
+    if (window.location.hash === "#contact") {
       this.$nextTick(() => {
         this.startScroll();
       });
